4cf001c67dbcb3e75a96b00604c9a3a35d827d68
danieldenton <danielmdenton@gmail.com>
2022-04-10 17:12:09 -0700

    title

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -5,6 +5,8 @@ export default function Layout(props) {
   return (
     <div className="layout">
       <Navbar />
+      <h1 className="dd">Daniel Denton</h1>
+      <h3>Full Stack Software Engineer</h3>
       <main className="main">{props.children}</main>
       <Footer />
     </div>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,6 @@
 export default function Home() {
   return (
     <div className="home">
-      <h1 className="dd">Daniel Denton</h1>
       <h2>Skills</h2>
       <h2>Recent Projects</h2>
     </div>
