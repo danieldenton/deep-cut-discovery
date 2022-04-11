a66f7a7bd518484cf042c7f8033d8aadccdab4d5
danieldenton <danielmdenton@gmail.com>
2022-04-10 18:24:27 -0700

    adding logos

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -5,8 +5,10 @@ export default function Layout(props) {
   return (
     <div className="layout">
       <Navbar />
-      <h1 className="dd">Daniel Denton</h1>
-      <h3>Full Stack Software Engineer</h3>
+      <header>
+        <h1 className="dd">Daniel Denton</h1>
+        <h3 className="sub-header">Full Stack Software Engineer</h3>
+      </header>
       <main className="main">{props.children}</main>
       <Footer />
     </div>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,17 @@
+import reactImg from "../../assets/react.png";
+import javascriptImg from "../../assets/javascript.png";
+
 export default function Home() {
   return (
     <div className="home">
-      <h2>Skills</h2>
-      <h2>Recent Projects</h2>
+      <div className="skills-div">
+        <h2>Skills</h2>
+        <img src={reactImg} alt="React" className="logos" />
+        <img src={javascriptImg} alt="Javascript" className="logos" />
+      </div>
+      <div className="projects-div">
+        <h2>Recent Projects</h2>
+      </div>
     </div>
   );
 }
