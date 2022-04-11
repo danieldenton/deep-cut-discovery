8a3b9199d79838c1ad684c8023a5414e59d11516
danieldenton <danielmdenton@gmail.com>
2022-04-10 19:42:09 -0700

    logos-started

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,8 +1,14 @@
 export default function About() {
   return (
     <div>
-      <h1>About:</h1>
-      <h3>I have a dik</h3>
+      <p className="bio">
+        Daniel Denton is a software engineer who completed General Assembly's
+        Software Engineering Emmersive in 2022. He has a background as a working
+        musician as well as yoga and meditation teacher. Whether a blank canvas
+        or an almost completed project, on a team or individually, he can be
+        counted on to bring whatever creative piece is needed to complete the
+        puzzle.
+      </p>
     </div>
   );
 }
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,16 +1,18 @@
 import reactImg from "../../assets/react.png";
 import javascriptImg from "../../assets/javascript.png";
+import nodeImg from "../../assets/node.png";
 
 export default function Home() {
   return (
     <div className="home">
       <div className="skills-div">
-        <h2>Skills</h2>
-        <img src={reactImg} alt="React" className="logos" />
+        <h2 className="page-header">Skills</h2>
+        <img src={reactImg} alt="React.js" className="logos" />
         <img src={javascriptImg} alt="Javascript" className="logos" />
+        <img src={nodeImg} alt="Node.js" className="logos" />
       </div>
       <div className="projects-div">
-        <h2>Recent Projects</h2>
+        <h2 className="page-header">Recent Projects</h2>
       </div>
     </div>
   );
